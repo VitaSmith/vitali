ca18ff483332ae147684a511844e8a1ef02b2274
VitaSmith <vitasmith@rpc1.org>
2017-11-01 02:46:03 +0000

    add Visual Studio solution files

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
-cd "%~dp0"
+cd /d "%~dp0"
 
 set CL=/nologo /errorReport:none /Gm- /GF /GS- /MT /MP /W4 /wd4324 /wd4996
 set LINK=/errorReport:none /INCREMENTAL:NO
